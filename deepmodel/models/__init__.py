017269bdef5d1bb7ce03709b45da9129efea7ced
lujie <lujie@4399inc.com>
2021-01-26 15:22:05 +0800

    git init

---
+++ deepmodel/models/__init__.py
@@ -0,0 +1,4 @@
+from .youtube import YouTubeNet
+from .deepfm import DeepFM
+
+__all__ = ["YouTubeNet", "DeepFM"]
---
+++ test/models/youtube_test.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+
+class YouTubeNet(object):
+
+    def __init__(self):
+        pass
+
+    def build_model(self):
+        pass
+
