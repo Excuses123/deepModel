0ac4dc2b05daa60acdbb4ce518820e53e9a30708
lujie <lujie@4399inc.com>
2021-01-26 15:30:22 +0800

    git init

--- deepmodel/models/__init__.py
+++ deepmodel/models/__init__.py
@@ -1,4 +1,5 @@
 from .youtube import YouTubeNet
 from .deepfm import DeepFM
+from .ffm import FFM
 
 __all__ = ["YouTubeNet", "DeepFM"]
---
+++ deepmodel/models/ffm.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+
+class FFM(object):
+
+    def __init__(self):
+        pass
+
+    def build_model(self):
+        pass
