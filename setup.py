0e22543b4db34d91c261b47f5ae78db96e3c5705
lujie <lujie@4399inc.com>
2022-07-06 17:16:42 +0800

    release 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deepModel",
-    version="0.0.2",
+    version="0.0.3",
     author="lujie",
     author_email="597906300@qq.com",
     description="A PyPi test project",
