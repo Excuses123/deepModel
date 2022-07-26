96c7909b4c62c659089e08a2a2631334620997ff
lujie <lujie@4399inc.com>
2022-07-26 13:42:31 +0800

    代码完善

--- deepmodel/core/__init__.py
+++ deepmodel/core/__init__.py
@@ -1,9 +1,6 @@
 
 from .layers import fc_layer, pool_layer, attention_layer
 from .activation import dice
-from .sequence import SequencePoolingLayer
-from .sequence import WeightedPoolingLayer
-from .sequence import AttentionSequencePoolingLayer
 from .sequence import Transformer
 
 
