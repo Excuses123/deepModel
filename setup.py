e1407407f8324b680b9788be2182e2d18d3f627c
lujie <lujie@4399inc.com>
2022-07-27 17:00:30 +0800

    release 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="deepModel",
-    version="0.0.8",
+    version="0.0.9",
     author="lujie",
     author_email="597906300@qq.com",
     description="This is a toolkit for deep learning development",
